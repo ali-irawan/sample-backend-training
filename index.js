7f424540e1538f572dacb0c76e52ef0028543062
Ali Irawan <boylevantz@gmail.com>
2020-02-15 00:02:51 +0700

    add code for login, books, POST book

---
+++ index.js
@@ -0,0 +1,96 @@
+require('dotenv').config()
+const authenticateJWT = require('./auth')
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const TOKEN_KEY = process.env.TOKEN_KEY
+
+// Some users
+const users = [
+  {
+    username: 'ali',
+    password: 'ali',
+    role: 'admin'
+  },
+  {
+    username: 'herry',
+    password: 'herry',
+    role: 'user'
+  }
+];
+
+const books = [
+  {
+      "author": "Chinua Achebe",
+      "country": "Nigeria",
+      "language": "English",
+      "pages": 209,
+      "title": "Things Fall Apart",
+      "year": 1958
+  },
+  {
+      "author": "Hans Christian Andersen",
+      "country": "Denmark",
+      "language": "Danish",
+      "pages": 784,
+      "title": "Fairy tales",
+      "year": 1836
+  },
+  {
+      "author": "Dante Alighieri",
+      "country": "Italy",
+      "language": "Italian",
+      "pages": 928,
+      "title": "The Divine Comedy",
+      "year": 1315
+  },
+];
+
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => res.send({ version: "1.0" }))
+
+app.post('/login', (req, res) => {
+  // Read username and password from request body
+  const { username, password } = req.body;
+
+  // Filter user from the users array by username and password
+  const user = users.find(u => { return u.username === username && u.password === password });
+
+  if (user) {
+      // Generate an access token
+      const accessToken = jwt.sign({ username: user.username,  role: user.role }, TOKEN_KEY);
+
+      res.json({
+          success: true,
+          accessToken
+      });
+  } else {
+      res.json({ success: false, error: "Username and password invalid" });
+  }
+});
+
+app.get('/books', authenticateJWT, (req, res) => {
+  res.json(books);
+});
+
+app.post('/books', authenticateJWT, (req, res) => {
+  const { role } = req.user;
+
+  if (role !== 'admin') {
+      return res.sendStatus(403);
+  }
+
+
+  const book = req.body;
+  books.push(book);
+
+  res.send('Book added successfully');
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
